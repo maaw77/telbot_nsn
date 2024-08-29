065d09a056cca7d7d7a6295c7870621265c48201
Гордеев Станислав Сергеевич <gsv001@gmkzoloto.ru>
2024-08-30 02:02:45 +0700

    Implemented ZabbixPoller

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-# import asyncio
-# import aiohttp
-# import yarl
-#
-# import logging
-#
-# URL_BASE = 'http://zabbix.gmkzoloto.ru/zabbix'
-#
-#
-# class ZabbixPoller:
-#     def __init__(self, url: str) -> None:
-#         self.url_base = yarl.URL(url)
-#
-#     def __repr__(self) -> str:
-#         return f'ZabbixPoller<url={self.url_base!s}>'
-#
-#
-# async def main():
-#     zb = ZabbixPoller(URL_BASE)
-#     print(zb)
-#     return True
-
-
 import logging
 import asyncio
 from asyncio import Queue
-
+from http import HTTPStatus
 from pprint import pprint
 
 import aiohttp
@@ -37,15 +14,44 @@ from telbot.settings import config
 from telbot import msgs_mngr
 
 
+URL_ZABBIX_BASE = 'http://zabbix.gmkzoloto.ru/zabbix'
 
+
+class ZabbixPoller:
+    def __init__(self, url: str) -> None:
+        self.url_base = URL(url)
+        self.url_api =  self.url_base/'api_jsonrpc.php'
+        self.auth: str = '' # uthentication token
+        self.id: int = 0 # an identifier of the corresponding request
+    
+    async def authontication(self, client: ClientSession) -> None:
+        data = {"jsonrpc":"2.0",
+                "method":"user.login",
+                "params": {"username": config.zabbix_username.get_secret_value(),
+                           "password": config.zabbix_password.get_secret_value()},
+                "id":1}
+        async with client.post(self.url_api, json=data) as resp:
+            assert resp.status == HTTPStatus.OK
+            data_for_auth = await resp.json()
+            self.auth = data_for_auth['result']
+            self.id = data_for_auth['id'] 
+            logging.info(data_for_auth)                
+
+
+    async def run(self) -> None:
+        """Launch the poller"""
+        async with aiohttp.ClientSession() as client:
+            await self.authontication(client)
+            while True:
+                await asyncio.sleep(0)
 
 
 async def main():
     queue_messages = Queue()  # Message queue for telegram bot.
     bot = tbot.TelBot(config.bot_token.get_secret_value(), queue_messages)
     msgs_manager = msgs_mngr.MessagesManager(queue_messages)
-
-    coroutines = (bot.run(), msgs_manager.run(), )
+    zbx_plr = ZabbixPoller(URL_ZABBIX_BASE)
+    coroutines = (bot.run(), msgs_manager.run(), zbx_plr.run())
     tasks = [asyncio.create_task(cor) for cor in coroutines]
     try:
         await asyncio.gather(queue_messages.join(), *tasks)
--- telbot/settings.py
+++ telbot/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=BASE_DIR/'.env', env_file_encoding='utf-8')
     bot_token: SecretStr
-
+    zabbix_username: SecretStr
+    zabbix_password: SecretStr
 
 config = Settings()
